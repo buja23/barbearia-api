61c5f075c5b98d9e103cba0477596f97a00ed4f2
buja23 <jooj.soares.227@gmail.com>
2026-04-02 17:57:37 -0300

    BarberEasy

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,7 +51,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
                 \App\Filament\Widgets\CalendarWidget::class,
             ])
 
