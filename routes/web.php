53abca8eade1e791de7c9b706d77cb8619c6533c
buja23 <jooj.soares.227@gmail.com>
2026-09-26 04:43:58 -0300

    mais testes nas rotas

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,30 @@ Route::get('/mp/callback', [MercadoPagoOAuthController::class, 'callback'])
 Route::get('/debug-session', function () {
     session(['teste_sessao' => 'funcionando']);
 
-    return response()->json([
-        'session_driver' => config('session.driver'),
-        'session_domain' => config('session.domain'),
-        'session_secure' => config('session.secure'),
-        'session_same_site' => config('session.same_site'),
-        'session_cookie' => config('session.cookie'),
-        'session_id' => session()->getId(),
-        'session_value' => session('teste_sessao'),
-        'request_secure' => request()->isSecure(),
-    ]);
+    $middlewares = app('router')
+        ->gatherRouteMiddleware(request()->route());
+
+    return response()
+        ->json([
+            'session_driver' => config('session.driver'),
+            'session_domain' => config('session.domain'),
+            'session_secure' => config('session.secure'),
+            'session_same_site' => config('session.same_site'),
+            'session_cookie' => config('session.cookie'),
+            'session_id' => session()->getId(),
+            'session_value' => session('teste_sessao'),
+            'request_secure' => request()->isSecure(),
+            'middlewares' => $middlewares,
+        ])
+        ->cookie(
+            'teste_cookie',
+            'funcionou',
+            10,
+            '/',
+            null,
+            true,
+            true,
+            false,
+            'lax'
+        );
 });
