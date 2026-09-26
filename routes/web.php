28b807146eec5a2ada667a04d5beea58d697b516
buja23 <jooj.soares.227@gmail.com>
2026-09-26 04:48:15 -0300

    testes nas rotas de cookies

--- routes/web.php
+++ routes/web.php
@@ -30,33 +30,10 @@ Route::get('/mp/disconnect', [MercadoPagoOAuthController::class, 'disconnect'])
 Route::get('/mp/callback', [MercadoPagoOAuthController::class, 'callback'])
     ->name('mp.callback');
 
-Route::get('/debug-session', function () {
-    session(['teste_sessao' => 'funcionando']);
-
-    $middlewares = app('router')
-        ->gatherRouteMiddleware(request()->route());
-
-    return response()
-        ->json([
-            'session_driver' => config('session.driver'),
-            'session_domain' => config('session.domain'),
-            'session_secure' => config('session.secure'),
-            'session_same_site' => config('session.same_site'),
-            'session_cookie' => config('session.cookie'),
-            'session_id' => session()->getId(),
-            'session_value' => session('teste_sessao'),
-            'request_secure' => request()->isSecure(),
-            'middlewares' => $middlewares,
-        ])
-        ->cookie(
-            'teste_cookie',
-            'funcionou',
-            10,
-            '/',
-            null,
-            true,
-            true,
-            false,
-            'lax'
+Route::get('/debug-cookie', function () {
+    return response('cookie teste')
+        ->header(
+            'Set-Cookie',
+            'manual_cookie=funcionou; Path=/; Secure; HttpOnly; SameSite=Lax'
         );
 });
