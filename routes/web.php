1fbcf7a9c82f76f108b3ce356a2c92a407afd979
buja23 <jooj.soares.227@gmail.com>
2026-09-26 04:38:19 -0300

    teste de rotas

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,17 @@ Route::get('/mp/disconnect', [MercadoPagoOAuthController::class, 'disconnect'])
 Route::get('/mp/callback', [MercadoPagoOAuthController::class, 'callback'])
     ->name('mp.callback');
 
+Route::get('/debug-session', function () {
+    session(['teste_sessao' => 'funcionando']);
 
+    return response()->json([
+        'session_driver' => config('session.driver'),
+        'session_domain' => config('session.domain'),
+        'session_secure' => config('session.secure'),
+        'session_same_site' => config('session.same_site'),
+        'session_cookie' => config('session.cookie'),
+        'session_id' => session()->getId(),
+        'session_value' => session('teste_sessao'),
+        'request_secure' => request()->isSecure(),
+    ]);
+});
